d252690bf75a88d1f282dac592267edad0da1827
karlosaurabh <saurabhsrivastava246@gmail.com>
2020-03-29 00:09:42 +0530

    created user and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { ChatBoxComponent } from './chat/chat-box/chat-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ChatBoxComponent
   ],
   imports: [
     BrowserModule,
