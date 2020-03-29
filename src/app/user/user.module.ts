7bb11a63428dd4256d2cb9e9fffc12174651f2aa
karlosaurabh <saurabhsrivastava246@gmail.com>
2020-03-29 22:34:21 +0530

    changes in chat box

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,4 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        CommonModule,
+
+    ],
+    providers: [],
+})
 export class UserModule {
 
 }
